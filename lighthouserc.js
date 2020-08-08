8531014bcac75894fce1a1164c25323b1db9e0d4
MATSUU Takuto <matsuu@gmail.com>
2020-08-08 10:05:30 +0900

    check results json

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,17 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
-      startServerCommand: 'yarn serve',
+      url: [
+        'http://localhost:3000/',
+        'http://localhost:3000/b0000',
+        'http://localhost:3000/b0006/entry/e0028',
+        'http://localhost:3000/b0004/entry/e0001',
+        'http://localhost:3000/404'
+      ],
+      startServerCommand: 'yarn serve'
     },
     upload: {
-      target: 'temporary-public-storage',
-    },
-  },
+      outputDir: './lhci'
+    }
+  }
 };
